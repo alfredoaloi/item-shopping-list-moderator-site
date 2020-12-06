053ed81532eaeda13f757442730f7c6471bf1678
Goffredson <alfredo_aloi@libero.it>
2020-12-06 16:57:39 +0100

    Almost finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 // Open login modal when open the page
 $(document).ready(function () {
-    console.log(document.cookie)
+    $('#items-select').prop('disabled', true);
+    $('#quantity').val(0)
+    $('#quantity').prop('disabled', true);
+    $('#change-quantity-button').prop('disabled', true);
+    $('#delete-item-button').prop('disabled', true);
+
     fetchUsers()
 });
 
@@ -13,13 +18,137 @@ function fetchUsers() {
         },
         success: function (response) {
             response.forEach(element => {
-                var option = document.createElement('option')
-                option.innerHTML = element.id + ' - ' + element.username
+                var option = '<option value="' + element.id + '">' + element.id + ' - ' + element.username + '</option>'
                 $('#users-select').append(option)
             });
+            $('#users-select').on('change', function () {
+                fetchItems($('#users-select').val())
+            })
         },
         error: function () {
             $('#login-modal').modal('show')
+        }
+    });
+}
+
+function fetchItems(id) {
+    if (id === '---') {
+        $('#items-select').empty()
+        var option = document.createElement('option')
+        option.innerHTML = '---'
+        $('#items-select').append(option)
+        $('#items-select').prop('disabled', true);
+        $('#quantity').val(0)
+        $('#quantity').prop('disabled', true);
+        $('#change-quantity-button').prop('disabled', true);
+        $('#delete-item-button').prop('disabled', true);
+        return
+    }
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8000/api/v1/shopping-list/mod/' + id + '/',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (response) {
+            $('#items-select').empty()
+            var option = document.createElement('option')
+            option.innerHTML = '---'
+            $('#items-select').append(option)
+            $('#quantity').val(0)
+            $('#quantity').prop('disabled', true);
+            $('#change-quantity-button').prop('disabled', true);
+            $('#delete-item-button').prop('disabled', true);
+            response.forEach(element => {
+                var option = '<option value="' + element.id + '">' + element.id + ' - ' + element.name + '</option>'
+                $('#items-select').append(option)
+            });
+            $('#items-select').prop('disabled', false);
+            $('#items-select').on('change', function () {
+                var id = $('#items-select').val()
+                var quantity = 0
+                response.forEach(element => {
+                    if (parseInt(element.id) === parseInt(id)) {
+                        quantity = element.quantity
+                    }
+                })
+                fetchQuantity(id, quantity)
+            })
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function fetchQuantity(id, quantity) {
+    if (id === '---') {
+        $('#quantity').val(0)
+        $('#quantity').prop('disabled', true);
+        $('#change-quantity-button').prop('disabled', true);
+        $('#delete-item-button').prop('disabled', true);
+        return
+    }
+    $('#quantity').prop('disabled', false);
+    $('#quantity').val(quantity);
+    $('#change-quantity-button').prop('disabled', false);
+    $('#delete-item-button').prop('disabled', false);
+}
+
+function changeQuantity() {
+    var id = $('#items-select').val()
+    var quantity = $('#quantity').val()
+    if (quantity < 1 || quantity > 5) {
+        // modale di errore
+        return
+    }
+    $.ajax({
+        type: 'PUT',
+        url: 'http://localhost:8000/api/v1/shopping-list/mod/edit/' + id + '/',
+        headers: {
+            'X-CSRFToken': cookieValue = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('csrftoken'))
+                .split('=')[1]
+        },
+        dataType: 'json',
+        contentType: 'application/json; charset=UTF-8',
+        data: JSON.stringify({
+            quantity: quantity
+        }),
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (response) {
+            location.reload()
+            // modale ok quantity
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function deleteItem() {
+    var id = $('#items-select').val()
+    $.ajax({
+        type: 'DELETE',
+        url: 'http://localhost:8000/api/v1/shopping-list/mod/edit/' + id + '/',
+        headers: {
+            'X-CSRFToken': cookieValue = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('csrftoken'))
+                .split('=')[1]
+        },
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (response) {
+            location.reload()
+            // modale ok delete
+        },
+        error: function () {
+
         }
     });
 }
@@ -63,11 +192,7 @@ function logout() {
             withCredentials: true
         },
         success: function (response) {
-            $('#users-select').empty()
-            var option = document.createElement('option')
-            option.innerHTML = '---'
-            $('#users-select').append(option)
-            $('#login-modal').modal('show')
+            location.reload()
         },
         error: function () {
             console.log('error logouting')
